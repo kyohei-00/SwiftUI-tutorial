8a3315d63b4d0d6da90a5f545474e54a2c93c3f7
坂本恭平 <kyohei.sakamoto00@gmail.com>
2022-01-23 14:11:52 +0900

    feat: section2まで

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(Color.green)
             .padding()
     }
 }
