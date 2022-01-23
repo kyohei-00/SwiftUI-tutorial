d44b1b8c90c6700a61b5b022fa3b40c554ff92b2
坂本恭平 <kyohei.sakamoto00@gmail.com>
2022-01-23 14:07:09 +0900

    init

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kyohei Sakamoto on 2022/01/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
