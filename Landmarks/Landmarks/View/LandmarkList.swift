49ada597da2a3cb32a9be2808289a7108af5f8f7
坂本恭平 <kyohei.sakamoto00@gmail.com>
2022-01-23 18:42:34 +0900

    feat: section04

---
+++ Landmarks/Landmarks/View/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Kyohei Sakamoto on 2022/01/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List{
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
