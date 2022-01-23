da353be45563d0a6dfdd5eb1c9bd7c3a0896e499
坂本恭平 <kyohei.sakamoto00@gmail.com>
2022-01-23 15:46:09 +0900

    feat: section04 image

---
+++ Landmarks/turtlerock@2x.swift
@@ -0,0 +1,20 @@
+//
+//  turtlerock@2x.swift
+//  Landmarks
+//
+//  Created by Kyohei Sakamoto on 2022/01/23.
+//
+
+import SwiftUI
+
+struct turtlerock_2x: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct turtlerock_2x_Previews: PreviewProvider {
+    static var previews: some View {
+        turtlerock_2x()
+    }
+}
