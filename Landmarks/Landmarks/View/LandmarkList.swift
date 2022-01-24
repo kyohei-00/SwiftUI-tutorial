0b0bae502e1512b96d17c48d71d5c11bb40b5660
坂本恭平 <kyohei.sakamoto00@gmail.com>
2022-01-24 18:23:02 +0900

    feat: section03

--- Landmarks/Landmarks/View/LandmarkList.swift
+++ Landmarks/Landmarks/View/LandmarkList.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
+    @State private var showFavoritesOnly = false
     
     var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in (!showFavoritesOnly || landmark.isFavorite)
@@ -18,14 +18,21 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationView{
-            List(filteredLandmarks){ landmark in
-                NavigationLink{
-                    LandmarkDetail(landmark: landmark)
-                } label:{
-                    LandmarkRow(landmark: landmark)
+            List{
+                Toggle(isOn: $showFavoritesOnly){
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks){ landmark in
+                    NavigationLink{
+                        LandmarkDetail(landmark: landmark)
+                    } label:{
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
+            
         }
     }
 }
