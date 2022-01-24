12723c3ab4dc641a99136cf48b8594142fefcd5d
坂本恭平 <kyohei.sakamoto00@gmail.com>
2022-01-24 18:03:20 +0900

    feat: section01

--- Landmarks/Landmarks/View/LandmarkRow.swift
+++ Landmarks/Landmarks/View/LandmarkRow.swift
@@ -16,6 +16,11 @@ struct LandmarkRow: View {
             Text(landmark.name)
             
             Spacer()
+            
+            if landmark.isFavorite{
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
