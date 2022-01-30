02ba2f244be4b96c6c5e0f8d15c8730b4dfa3b12
坂本恭平 <kyohei.sakamoto00@gmail.com>
2022-01-30 21:41:13 +0900

    feat: section04

--- Landmarks/Landmarks/Model/Landmark.swift
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -16,6 +16,7 @@ struct Landmark: Hashable, Codable, Identifiable {
     var state: String
     var description: String
     var isFavorite: Bool
+    var isFeatured: Bool
     
     var category: Category
     enum Category: String, CaseIterable,Codable{
@@ -23,19 +24,19 @@ struct Landmark: Hashable, Codable, Identifiable {
         case rivers = "Rivers"
         case mountains = "Mountains"
     }
-
+    
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
-
+    
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
             longitude: coordinates.longitude)
     }
-
+    
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
