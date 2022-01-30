3f69e98fc29788fdeb8eef2babecbb53c9028ce4
坂本恭平 <kyohei.sakamoto00@gmail.com>
2022-01-31 00:02:44 +0900

    feat: section04

--- Landmarks/Landmarks/View/ProfileHost.swift
+++ Landmarks/Landmarks/View/ProfileHost.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20){
-            ProfileSummary(profile: draftProfile)
+            HStack{
+                if editMode?.wrappedValue == .active {
+                    if #available(iOS 15.0, *) {
+                        Button("Cancel", role:.cancel){
+                            draftProfile = modelData.profile
+                            editMode?.animation().wrappedValue = .inactive
+                        }
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                }
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear{
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear{
+                        modelData.profile = draftProfile
+                    }
+            }
         }
         .padding()
     }
@@ -21,5 +48,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
