a52b1993a395dddbbfc96779308f8b5ac583f4c9
坂本恭平 <kyohei.sakamoto00@gmail.com>
2022-01-30 23:26:48 +0900

    feat: ch07

--- Landmarks/Landmarks/View/ContentView.swift
+++ Landmarks/Landmarks/View/ContentView.swift
@@ -8,7 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
+        TabView(selection: $selection){
+            CategoryHome()
+                .tabItem{
+                    Label("Featured",systemImage: "star")
+                }
+                .tag(Tab.featured)
+            
+            LandmarkList()
+                .tabItem{
+                    Label("List",systemImage: "list.buillet")
+                }
+                .tag(Tab.list)
+        }
         LandmarkList()
     }
 }
