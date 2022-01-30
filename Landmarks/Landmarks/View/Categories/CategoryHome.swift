1ab0376d02d07f039691ffed830515487e3596f7
坂本恭平 <kyohei.sakamoto00@gmail.com>
2022-01-30 21:04:52 +0900

    feat: ch06/section01

---
+++ Landmarks/Landmarks/View/Categories/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Kyohei Sakamoto on 2022/01/30.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+        
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
